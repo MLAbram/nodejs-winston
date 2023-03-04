d7a363c896792d03878424d889a9435be404ff34
MLAbram <23126920+MLAbram@users.noreply.github.com>
2023-03-03 19:17:36 -0600

    code updates

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const logger = require('./routes/winston');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  logger.error('Error');
+  logger.warn('Warn');
+  logger.info('Info');
+  logger.http('HTTP');
+  logger.verbose('Verbose');
+  logger.debug('Debug');
+  logger.silly('Silly');
+
+  return res.status(200).send('Error, Warn, Info, HTTP, Verbose, Debug, Silly');
+});
+
+app.get('/error', (req, res) => {
+  logger.error('Error', {
+    headersHost: req.headers['host'],
+    headersLanguage: req.headers['accept-language'],
+    headersSecMobile: req.headers['sec-ch-ua-mobile'],
+    headersPlatform: req.headers['sec-ch-ua-platform'],
+    headersAgent: req.headers['user-agent']
+  });
+  
+  return res.status(200).send('Error');
+});
+
+app.get('/warn', (req, res) => {
+  logger.warn('Warn');
+  
+  return res.status(200).send('Warn');
+});
+
+app.get('/info', (req, res) => {
+  logger.info('Info');
+  
+  return res.status(200).send('Info');
+});
+
+app.get('/http', (req, res) => {
+  logger.http('HTTP');
+  
+  return res.status(200).send('HTTP');
+});
+
+app.get('/verbose', (req, res) => {
+  logger.verbose('Verbose');
+  
+  return res.status(200).send('Verbose');
+});
+
+app.get('/debug', (req, res) => {
+  logger.debug('Debug');
+  
+  return res.status(200).send('Debug');
+});
+
+app.get('/silly', (req, res) => {
+  logger.silly('Silly');
+  
+  return res.status(200).send('Silly');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port: 3000.');
+});
---
+++ routes/winston.js
@@ -0,0 +1,46 @@
+// https://github.com/winstonjs/winston
+// https://www.npmjs.com/package/winston
+// https://www.npmjs.com/package/winston-mongodb
+
+// https://betterstack.com/community/guides/logging/how-to-install-setup-and-use-winston-and-morgan-to-log-node-js-applications/
+// https://www.section.io/engineering-education/logging-with-winston/
+// https://dev.to/jobizil/getting-started-with-winston-logger-a-beginner-s-guide-7j3
+
+// https://www.youtube.com/watch?v=PdVlAi7nrRU
+// https://github.com/Tariqu/winston_logger_example
+
+
+const { createLogger, transports, format } = require('winston');
+const { combine, timestamp, json, printf } = format;
+const dotenv = require('dotenv').config();
+
+require('winston-mongodb');
+require('winston-daily-rotate-file');
+
+const logger = createLogger({
+    defaultMeta: {service: 'admin-service'},
+    transports: [
+        new transports.Console(),
+        new transports.DailyRotateFile({
+            level: 'info',
+            filename: 'logs/winston-info-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            maxFiles: '7d',
+            format: format.combine(timestamp(), format.json())
+        }),
+        new transports.File({
+            level: 'silly',
+            filename: 'logs/winston-prettyPrint.log',
+            format: format.combine(timestamp(), format.prettyPrint())
+        }),
+        new transports.MongoDB({
+            level: 'error',
+            db: process.env.MONGODB,
+            options: { useNewUrlParser: true, useUnifiedTopology: true },
+            collection: 'nodejs_winston',
+            format: format.combine(format.timestamp(), format.json())
+        })
+    ]
+});
+
+module.exports = logger;
